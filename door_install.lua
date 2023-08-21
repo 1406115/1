f623153624844c38070bda85a66649799a503070
1406115 <91167226+1406115@users.noreply.github.com>
2023-08-21 01:54:56 -0400

    Update door_install.lua

--- door_install.lua
+++ door_install.lua
@@ -1,2 +1,3 @@
+-- wget run 
 shell.run("wget https://raw.githubusercontent.com/1406115/1/main/door.lua?token=GHSAT0AAAAAACGRHIFKQSTLL455PHCKAQJQZHC7NTQ door")
 shell.run("wget https://raw.githubusercontent.com/1406115/1/main/door2.lua?token=GHSAT0AAAAAACGRHIFKNYSBFU6AXJ7NRHVIZHC7PEA startup")
