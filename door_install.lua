eb14bef8a5d4b16cd4b96952e6372816392a5e4a
1406115 <91167226+1406115@users.noreply.github.com>
2023-08-21 02:14:28 -0400

    Update door_install.lua

--- door_install.lua
+++ door_install.lua
@@ -1,3 +1,3 @@
-shell.run("wget https://raw.githubusercontent.com/1406115/1/main/door.lua not_door")
+shell.run("wget https://raw.githubusercontent.com/1406115/1/main/door.lua Door")
 shell.run("wget https://raw.githubusercontent.com/1406115/1/main/door2.lua startup")
 shell.run("startup")
