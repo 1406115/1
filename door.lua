36414dccd54aebf37eedf59e27d033fb46e86825
1406115 <91167226+1406115@users.noreply.github.com>
2023-08-21 01:49:41 -0400

    Add files via upload

--- door.lua
+++ door.lua
@@ -1,15 +1,14 @@
 local detector = peripheral.find("playerDetector") -- calls the playerDetector
-local pos1  = { x = -88 , y = -59 , z = 59}
-local pos2  = { x = -89 , y = -56 , z = 61}
-while true do 
-player = detector.isPlayerInCoords(pos1 , pos2 , "potato1000000000")
-if player == true then do 
+local pos1  = {x = -88,y = -59,z = 59} -- first set of cords 
+local pos2  = {x = -89,y = -56,z = 61} -- second set of cords
+while true do -- loops it so it always checks 
+player = detector.isPlayerInCoords(pos1 , pos2 , "potato1000000000")-- checks for just me 
+sleep(0.01) -- prevents it from having an error 
+if player == true then do -- if im at the door open
 rs.setOutput("top",true)
-write("true")
 end
-else do 
+else do -- if im not at the door close
 rs.setOutput("top",false)
-write("false")
 end
 end
 end 
