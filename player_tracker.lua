e20fea1ea4ffca4b9e0e9c3aac36ee082be5f730
1406115 <91167226+1406115@users.noreply.github.com>
2023-08-23 03:01:19 -0400

    Add files via upload

---
+++ player_tracker.lua
@@ -0,0 +1,8 @@
+local tArgs = { ... }
+local player = ( tArgs[1] )
+local detector = peripheral.find("playerDetector")
+while true do
+term.setCursorPos(1,0)
+local pos = detector.getPlayerPos(player)
+print("\n",player,"'s", "cords","\nx",pos.x,"\ny",pos.y,"\nz",pos.z)
+end
