b68f3d95300f023a60e955d33a4fb1182f30b507
1406115 <91167226+1406115@users.noreply.github.com>
2023-08-22 23:14:32 -0400

    Update door.lua

--- door.lua
+++ door.lua
@@ -3,7 +3,7 @@ local gpsx, gpsy, gpsz = gps.locate(5)
 local pos1  = {x = gpsx + 2 ,y = gpsy + 1,z = gpsz - 1} -- first set of cords 
 local pos2  = {x = gpsx - 1 ,y = gpsy + 4,z = gpsz + 2} -- second set of cords
 while true do -- loops it so it always checks 
-player = detector.isPlayersInCoords(pos1 , pos2 )-- checks for just me 
+player = detector.isPlayersInCoords(pos1 , pos2 )-- checks for just players
 sleep(0.01) -- prevents it from having an error 
 if player == true then do -- if im at the door open
 rs.setOutput("top",true)
