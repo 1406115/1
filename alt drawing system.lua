5e9a395b3d2d55aba216208fcf7677b30c76efc9
myce the mushroom <potato120000000@gmail.com>
2024-06-21 00:45:08 -0400

    Update alt drawing system.lua

--- alt drawing system.lua
+++ alt drawing system.lua
@@ -18,7 +18,6 @@ else
   textutils.slowPrint(draw)
   table.remove (deck,value)
  end
- if tArgs[2] ~= "cheats_on" then
   value = math.random(1,#deck)
  term.setTextColor( prime_color[ tArgs[1] ] )
  textutils.slowPrint("Starting Hand:") 
@@ -47,34 +46,6 @@ else
    end
   end
  end
- else
-  value = 0
-  e = 0
-  term.setTextColor( prime_color[ tArgs[1] ] )
- textutils.slowPrint("Starting Hand:") 
- term.setTextColor( second_color[ tArgs[1] ] ) 
-  for i=1,5 do
-  repeat
-   value = e + 1
-   until(deck[value] ~= nil)
-   draw_from_deck(deck)
-  end
- while true do
-  local event, key, is_held = os.pullEvent("key")
-  if key == keys.space then
-   if deck[1] == nil and deck[2] == nil and deck[3] == nil and deck[4] == nil and deck[5] == nil and deck[6] == nil and deck[7] == nil and deck[8] == nil and deck[9] == nil and deck[10] == nil and deck[11] == nil and deck[12] == nil and deck[13] == nil and deck[14] == nil and deck[15] == nil and deck[16] == nil and deck[17] == nil and deck[18] == nil and deck[19] == nil and deck[20] == nil and deck[21] == nil and deck[22] == nil and  deck[23] == nil and deck[24] == nil and deck[25] == nil and deck[26] == nil and deck[27] == nil and deck[28] == nil and deck[29] == nil and deck[30] == nil and deck[31] == nil and deck[32] == nil and deck[33] == nil and deck[34] == nil and deck[35] == nil and deck[36] == nil and deck[37] == nil and  deck[38] == nil and deck[39] == nil and deck[40] == nil and deck[41] == nil and deck[42] == nil and deck[43] == nil and deck[44] == nil and deck[45] == nil and deck[46] == nil and deck[47] == nil and deck[48] == nil and deck[49] == nil and deck[50] == nil and deck[51] == nil and deck[52] == nil and deck[53] == nil and deck[54] == nil and deck[55] == nil and deck[56] == nil and deck[57] == nil and deck[58] == nil and deck[59] == nil and deck[60] == nil then
-    term.setTextColor( colors.red )
-    textutils.slowPrint("YOU CHEATED",2)
-    break
-   else
-    repeat
-     value = e + 1
-    until(deck[value] ~= nil)
-     term.setTextColor( prime_color[ tArgs[1] ] )
-     textutils.slowPrint("You drew:")
-     term.setTextColor( second_color[ tArgs[1] ] )
-     draw_from_deck(deck)
-   end
   end
  end
 end
