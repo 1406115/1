cf42e0618738facc054b7d3a999b627bf6ec76c9
myce the mushroom <potato120000000@gmail.com>
2024-06-22 20:48:43 -0400

    Update dice.lua

--- dice.lua
+++ dice.lua
@@ -5,5 +5,13 @@ while true do
   if key == keys.space then
     value = math.random(1,d20)
     print("you rolled" value)
+ else 
+  if key == keys.up 
+   value = math.random(10,d20)
+   print("you rolled" value)
+  else 
+  if key == keys.down 
+   value = 20
+   print("you rolled" value)
   end
 end
