79416c80aaf4fe9085ac6be98422b0c8365612a0
myce the mushroom <potato120000000@gmail.com>
2024-06-22 20:50:38 -0400

    Update dice.lua

--- dice.lua
+++ dice.lua
@@ -4,15 +4,15 @@ while true do
  local event, key, is_held = os.pullEvent("key")
   if key == keys.space then
     value = math.random(1,d20)
-    print("you rolled" value)
+    print("you rolled", value)
  else 
   if key == keys.up 
    value = math.random(10,d20)
-   print("you rolled" value)
+   print("you rolled", value)
   else 
   if key == keys.down 
    value = 20
-   print("you rolled" value)
+   print("you rolled", value)
   end
   end
  end
